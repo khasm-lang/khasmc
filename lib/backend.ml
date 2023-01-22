6855f64889929d1cd6bc939ea1de89cfdb709d41
jake-87 <jakehfreebsd@gmail.com>
2023-01-22 14:49:34 +1100

    Added complexity, formatted with ocamlformat

--- lib/backend.ml
+++ lib/backend.ml
@@ -3,67 +3,41 @@ open Ast
 open Exp
 open Typecheck
 
+let ( -- ) i j =
+  let rec aux n acc = if n < i then acc else aux (n - 1) (n :: acc) in
+  aux j []
 
-let (--) i j = 
-    let rec aux n acc =
-      if n < i then acc else aux (n-1) (n :: acc)
-    in aux j []
+let table = 0 -- 255 |> List.map (Printf.sprintf "%2X")
 
-let table =
-  0 -- 255
-  |> List.map (Printf.sprintf "%2X")
-
-let string_of_chars chars = 
+let string_of_chars chars =
   let buf = Buffer.create 16 in
   List.iter (Buffer.add_char buf) chars;
   Buffer.contents buf
 
-let chars_of_string s =
-  List.init (String.length s) (String.get s)
-
+let chars_of_string s = List.init (String.length s) (String.get s)
 let mangle_c c = List.nth table c
+let mangle str = if str = "()" then "Khasmc_int.bottom" else "Khasmc." ^ str
 
+type scope = { binds : (string * string) list }
+[@@deriving show { with_path = false }]
 
-let mangle str =
-  if str = "()" then "Khasmc_int.bottom" else
-    "Khasmc." ^ str
-
-
-type scope = {
-    binds: (string * string) list;
-  }
-[@@deriving show {with_path = false}]
-
-let new_scope () =
-  {binds = ("()", "()") :: []}
-
-let add_bind scp id bd =
-  {binds = (id, bd) :: scp.binds}
-
+let new_scope () = { binds = ("()", "()") :: [] }
+let add_bind scp id bd = { binds = (id, bd) :: scp.binds }
 let get_bind scp str = List.assoc str scp
-
 let paren x = "\n(" ^ x ^ ")"
 let brack x = " [" ^ x ^ "] "
 let brace x = " {" ^ x ^ "} "
 
 let rec join_on s xs =
-  match xs with
-  | [] -> ""
-  | [x] -> x
-  | x :: xs -> x ^ s ^ join_on s xs
+  match xs with [] -> "" | [ x ] -> x | x :: xs -> x ^ s ^ join_on s xs
 
-let (<|) f x = f x
-
-let rec dup s ind =
-  match ind with
-  | 0 -> ""
-  | x -> s ^ dup s (x - 1)
+let ( <| ) f x = f x
+let rec dup s ind = match ind with 0 -> "" | x -> s ^ dup s (x - 1)
 
 let gen ind x =
   if String.starts_with ~prefix:"\n" x then
     "\n" ^ dup "  " ind ^ String.sub x 1 (String.length x - 1)
-  else
-    dup "  " ind ^ x
+  else dup "  " ind ^ x
 
 (*
   the grammar of this is basically
@@ -88,7 +62,6 @@ let gen ind x =
   | ifelse: (ifelse  (cond) (e1) (e2))
 *)
 
-
 (*
   The decl of the pprinter for types is in ast.ml, because
   ocaml doesn't support recursive modules :(
@@ -96,91 +69,69 @@ let gen ind x =
 
 let rec codegen_base b scope ind =
   match b with
-  | Ident(_, t) ->
-     "bound " ^  
-       begin
-         try
-           List.assoc t scope.binds
-         with
-         | Not_found -> mangle t
-       end |> paren |> gen ind
-  | Int(s) -> "int " ^ s |> paren |> gen ind
-  | Float(s) -> "float "  ^ s |> paren |> gen ind
-  | Str(s) -> "string \"" ^ s ^ "\"" |> paren |> gen ind
-  | Tuple(ex) -> "tuple "
-                 ^ join_on " "
-                     (List.map (fun x -> codegen_expr x scope (ind + 1)) ex)
-                 |> paren |> gen ind
+  | Ident (_, t) ->
+      ("bound " ^ try List.assoc t scope.binds with Not_found -> mangle t)
+      |> paren |> gen ind
+  | Int s -> "int " ^ s |> paren |> gen ind
+  | Float s -> "float " ^ s |> paren |> gen ind
+  | Str s -> "string \"" ^ s ^ "\"" |> paren |> gen ind
+  | Tuple ex ->
+      "tuple "
+      ^ join_on " " (List.map (fun x -> codegen_expr x scope (ind + 1)) ex)
+      |> paren |> gen ind
   | True -> "bool true" |> paren |> gen ind
   | False -> "bool false" |> paren |> gen ind
 
-
-
 and codegen_expr ex scope ind =
   match ex with
-  | Base(_, k) -> codegen_base k scope (ind)
-  | FCall(_, f, x) -> "fcall "
-                      ^ codegen_expr f scope (ind + 1)
-                      ^ codegen_expr x scope (ind + 1)
-                      |> paren |> gen ind
-  | AnnotLet(_, l, _, e1, e2)
-    | LetIn(_, l, e1, e2) -> "let "
-                             ^   mangle l
-                             ^ codegen_expr e1 scope(ind + 1)
-                             ^ codegen_expr e2 scope (ind + 1)
-                             |> paren |> gen ind
+  | Base (_, k) -> codegen_base k scope ind
+  | FCall (_, f, x) ->
+      "fcall " ^ codegen_expr f scope (ind + 1) ^ codegen_expr x scope (ind + 1)
+      |> paren |> gen ind
+  | AnnotLet (_, l, _, e1, e2) | LetIn (_, l, e1, e2) ->
+      "let " ^ mangle l
+      ^ codegen_expr e1 scope (ind + 1)
+      ^ codegen_expr e2 scope (ind + 1)
+      |> paren |> gen ind
+  | Join (_, e1, e2) ->
+      "seq " ^ codegen_expr e1 scope (ind + 1) ^ codegen_expr e2 scope (ind + 1)
+      |> paren |> gen ind
+  | Inst (_, _, _) -> raise <| Impossible "INST"
+  | IfElse (_, c, e1, e2) ->
+      "ifelse "
+      ^ codegen_expr c scope (ind + 1)
+      ^ codegen_expr e1 scope (ind + 1)
+      ^ codegen_expr e2 scope (ind + 1)
+      |> paren |> gen ind
+  | Lam (_, l, e) | AnnotLam (_, l, _, e) ->
+      "lam " ^ mangle l ^ codegen_expr e scope (ind + 1) |> paren |> gen ind
+  | TupAccess (_, e, i) ->
+      "tupaccess " ^ string_of_int i ^ codegen_expr e scope (ind + 1)
+      |> paren |> gen ind
+  | TypeLam (_, _, e) -> codegen_expr e scope ind
 
-  | Join(_, e1, e2) -> "seq "
-                       ^ codegen_expr e1 scope(ind + 1)
-                       ^ codegen_expr e2 scope (ind + 1)
-                       |> paren |> gen ind
-
-  | Inst(_, _, _) -> raise <| Impossible "INST"
-  | IfElse(_, c, e1, e2) -> "ifelse "
-                            ^ codegen_expr c scope (ind + 1)
-                            ^ codegen_expr e1 scope(ind + 1)
-                            ^ codegen_expr e2 scope (ind + 1)
-                            |> paren |> gen ind
-  | Lam(_, l, e)
-    | AnnotLam(_, l, _, e) -> "lam "
-                              ^ mangle l
-                              ^ codegen_expr e scope (ind + 1)
-                              |> paren |> gen ind
-  | TupAccess(_, e, i) -> "tupaccess "
-                          ^ string_of_int i
-                          ^ codegen_expr e scope (ind + 1)
-                          |> paren |> gen ind
-
-  | TypeLam(_, _, e) -> codegen_expr e scope ind
-    
 let codegen_assign a e scope =
-  let (id, _) = a in
-  let (_, args, body) = e in
+  let id, _ = a in
+  let _, args, body = e in
   let body' = body in
-  begin
-    "tlet "
-    ^ mangle id ^ " "
-    ^ paren (join_on " " (List.map mangle args))
-    ^ (codegen_expr body' scope 0)
-    |> paren
-  end
+  ("tlet " ^ mangle id ^ " "
+   ^ paren (join_on " " (List.map mangle args))
+   ^ codegen_expr body' scope 0
+  |> paren)
   ^ "\n"
 
 let rec codegen_program p s =
-  let scp = match s with
-    | Some(x) -> x
-    | None -> new_scope ()
-  in 
+  let scp = match s with Some x -> x | None -> new_scope () in
   match p with
-  | Program([]) -> ""
-  | Program(x :: xs) ->
-     let scp'str = match x with
-       | TopAssign(a, e) -> (add_bind scp (fst a) (mangle (fst a)),
-                             codegen_assign a e scp)
-       | Extern(id, _) -> (add_bind scp id id, "\n(extern " ^ id ^ ")\n")
-     in
-     snd scp'str ^ codegen_program (Program(xs)) (Some(fst scp'str))
-     
+  | Program [] -> ""
+  | Program (x :: xs) ->
+      let scp'str =
+        match x with
+        | TopAssign (a, e) ->
+            (add_bind scp (fst a) (mangle (fst a)), codegen_assign a e scp)
+        | Extern (id, _) -> (add_bind scp id id, "\n(extern " ^ id ^ ")\n")
+      in
+      snd scp'str ^ codegen_program (Program xs) (Some (fst scp'str))
 
 let rec codegen names programs =
   match programs with
--- lib/main.ml
+++ lib/main.ml
@@ -17,62 +17,59 @@ let parseToAst filename =
     let result = Parser.program Lexer.token lexbuf in
     close_in file;
     result
-  with
-  | Parser.Error(x) ->
-     let error_msg = Fmt.str "%s: syntax error in state %d@." (print_error_position lexbuf) x in
-     print_endline ("Parse error: " ^ error_msg);
-     exit 1
+  with Parser.Error x ->
+    let error_msg =
+      Fmt.str "%s: syntax error in state %d@." (print_error_position lexbuf) x
+    in
+    print_endline ("Parse error: " ^ error_msg);
+    exit 1
 
 let rec normalise files =
   match files with
   | [] -> []
   | x :: xs ->
-     (
-       Filename.basename x
-       |> Filename.chop_extension
-       |> BatString.capitalize
-     )  :: normalise xs
-
+      (Filename.basename x |> Filename.chop_extension |> BatString.capitalize)
+      :: normalise xs
 
 let compile names asts =
-  (*typcheck stage*)
-  asts
-  |> List.map Typelam_init.init_program
-  |> typecheck_program_list;
-  (*codegen stage*)
-  asts
-  |> List.iter (fun x -> Debug.debug (show_program x)); asts
-  |> Backend.codegen names
+  let asts' = asts |> List.map Complexity.init_program in
 
+  (*typcheck stage*)
+  asts' |> List.map Typelam_init.init_program |> typecheck_program_list;
+  (*codegen stage*)
+  asts' |> List.iter (fun x -> Debug.debug (show_program x));
+  asts |> Backend.codegen names
 
 let main_proc () =
   let () = Printexc.record_backtrace true in
   let argc = Array.length Sys.argv in
-  if argc < 2 then
-    begin
-      print_endline "USAGE: ./khasmc <file>";
-      exit 1
-    end;
+  if argc < 2 then (
+    print_endline "USAGE: ./khasmc <file>";
+    exit 1);
   print_endline "";
-  let t = Unix.gettimeofday() in
-  let succ = begin
+  let t = Unix.gettimeofday () in
+  let succ =
     try
       let list = Array.to_list Sys.argv in
-      let files = List.tl list in (* make this better *)
+      let files = List.tl list in
+      (* make this better *)
       let programs = List.map parseToAst files in
-      let names = normalise files in 
+      let names = normalise files in
       let res = compile names programs in
       print_endline res;
       "Success"
     with
-    | TypeErr(x) -> ("Caught TypeErr:\n" ^ x)
-    | NotFound(x) -> ("Caught NotFound:\n" ^ x)
-    | NotImpl(x) -> ("NOTIMPL:\n" ^ x)
-    | UnifyErr(x) -> ("Caught UnifyErr:\n" ^ x)
-    end
+    | TypeErr x -> "Caught TypeErr:\n" ^ x
+    | NotFound x -> "Caught NotFound:\n" ^ x
+    | NotImpl x -> "NOTIMPL:\n" ^ x
+    | UnifyErr x -> "Caught UnifyErr:\n" ^ x
   in
   Debug.log_debug_stdout false;
   print_endline ("\nStatus: " ^ succ);
-  print_endline ("Used " ^ string_of_int !uniq ^ " typvars, " ^ string_of_int !muniq ^ " metavars and " ^ string_of_int (getid () - 1) ^ " nodes");
-  Printf.printf "\nkhasmc done in %fs\n"  ((Unix.gettimeofday()) -. t);
+  print_endline
+    ("Used " ^ string_of_int !uniq ^ " typvars, " ^ string_of_int !muniq
+   ^ " metavars and "
+    ^ string_of_int (getid () - 1)
+    ^ " nodes");
+  Printf.printf "\nkhasmc done in %fs\n" (Unix.gettimeofday () -. t);
   ()
