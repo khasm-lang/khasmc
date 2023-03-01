4161b2f0a7bc75c548eae5c302cdc16a1f1de2c4
jake-87 <jakehfreebsd@gmail.com>
2023-03-01 21:57:28 +1100

    bugfixes and worked on backend runtime

--- lib/backend/khagm.ml
+++ lib/backend/khagm.ml
@@ -1,1 +1,15 @@
 open Exp
+
+type khagmid = int
+type unboxed = Int' of string | String' of string | Float' of string
+
+type khagmexpr =
+  | Val of khagmid
+  | Unboxed of unboxed
+  | Tuple of khagmexpr list
+  | Call of khagmexpr * khagmexpr
+  | Eval of khagmexpr
+
+type khagmtop =
+  | Let of khagmid * khagmid list * khagmexpr
+  | Extern of khagmid * string
