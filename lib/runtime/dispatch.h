a2749e5457025310ab930e2ccb98663984c53595
jake-87 <>
2023-03-15 17:36:51 +1100

    remove backup

--- lib/runtime/dispatch.h
+++ lib/runtime/dispatch.h
@@ -4,6 +4,5 @@
 #include "err.h"
 #include "khagm_obj.h"
 #include "khagm_alloc.h"
-khagm_obj * reconcile(khagm_obj * ret, khagm_obj ** args,
-		      int arity, int argnum);
+
 khagm_obj * dispatch(int, fptr, khagm_obj **);
