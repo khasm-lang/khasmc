49368e2a997ad44b612f2026269d3c9723a43a39
jake-87 <jakehfreebsd@gmail.com>
2023-04-28 20:56:46 +1000

    ADT in progress

--- docs/src/README.md
+++ docs/src/README.md
@@ -2,5 +2,5 @@
 
 This page contains both documentation for the Khasm compiler, and the Khasm standard library.
 
-- Standard library docs: [./stdlib/overview.md]
-- Compiler docs: [./compiler/overview.md]
+- Standard library docs: [./stdlib/overview.md](stdlib overview)
+- Compiler docs: [./compiler/overview.md](compiler overview)
--- lib/runtime/khagm_obj.c
+++ lib/runtime/khagm_obj.c
@@ -5,6 +5,8 @@
 #include "khagm_obj.h"
 #include "khagm_eval.h"
 
+
+
 #define packed __attribute__ ((packed))
 
 
@@ -23,15 +25,13 @@ khagm_obj * set_used(khagm_obj *a, i32 b) {
   return a;
 }
 khagm_obj * set_gc(khagm_obj *a, i8 b) {
-  i32 mask = ((1 << 24) - 1) << 8;
-  i32 first24 = a->used & mask;
-  a->used = b & (first24 << 8);
+  a->gc = b;
   return a;
 }
 
 i32 get_used(khagm_obj *a) { return a->used; }
 
-i8 get_gc(khagm_obj *a) { return a->used & (1 << 8) - 1; }
+u64 get_gc(khagm_obj *a) { return a->gc; }
 
 khagm_obj * khagm_obj_copy_thunk(khagm_obj * o) {
   khagm_obj * new = k_alloc(sizeof(khagm_obj));
