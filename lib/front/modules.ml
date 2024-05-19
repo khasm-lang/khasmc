6e6cc3634b36b1860812e3d8dd2b53c0341c2edd
jake-87 <jakehfreebsd@gmail.com>
2024-05-19 20:29:19 +1000

    _

--- lib/front/modules.ml
+++ lib/front/modules.ml
@@ -13,7 +13,8 @@ type ctx = {
 [@@deriving show { with_path = false }]
 
 (* returns only the full matches as their longest path *)
-let full_matches (paths : (path * path) list) (path : path) : path list =
+let full_matches (paths : (path * path) list) (path : path) :
+    path list =
   let rec go p1 p2 =
     match (p1, p2) with
     | End, End -> true
@@ -33,14 +34,15 @@ let rec similar_up_to path1 path2 =
   | InMod (a, q), InMod (b, w) when a = b -> similar_up_to q w
   | _ -> None
 
-let rec find_naive_definition
-    (ctx : ctx) (path : path) : (path, 'a) result =
+let rec find_naive_definition (ctx : ctx) (path : path) :
+    (path, 'a) result =
   match path with
-  | End -> err (`Invalid_Argument "Got End where full path was expected")
+  | End ->
+      err (`Invalid_Argument "Got End where full path was expected")
   | Base n -> (
       match
-        full_matches
-          ctx.definitions (InMod (ctx.current, Base n)) with
+        full_matches ctx.definitions (InMod (ctx.current, Base n))
+      with
       | [] -> err (`No_Such_Variable path)
       | [ x ] -> ok x
       | _ -> err (`Overlapping_Variable path))
@@ -59,7 +61,8 @@ let handle_opens (ctx : ctx) : ctx =
   in
   let new_defs =
     List.fold_left
-      (fun acc nm -> remove_one nm acc) ctx.definitions ctx.open'
+      (fun acc nm -> remove_one nm acc)
+      ctx.definitions ctx.open'
   in
   { ctx with definitions = new_defs; open' = [] }
 
@@ -69,11 +72,13 @@ let ensure_includes_correct ctx path =
   | Base _ -> ok path
   | _ -> (
       List.map (fun x -> similar_up_to x path) ctx.included
-      |> List.filter (fun x -> match x with Some _ -> true | None -> false)
+      |> List.filter (fun x ->
+             match x with Some _ -> true | None -> false)
       |> function
       | [] ->
-        err
-          (`Bad_Include ("No such path wrt includes: " ^ show_path path))
+          err
+            (`Bad_Include
+              ("No such path wrt includes: " ^ show_path path))
       | _ -> ok path)
 
 let find_definition (ctx : ctx) (path : path) : (path, 'a) result =
@@ -81,42 +86,44 @@ let find_definition (ctx : ctx) (path : path) : (path, 'a) result =
   let ctx = handle_opens ctx in
   find_naive_definition ctx path |=> ensure_includes_correct ctx
 
-let handle_definition (ctx: ctx)
-    {name; free_vars; constraints; args; ret; body}: definition =
-  
+let handle_definition (ctx : ctx)
+    { name; free_vars; constraints; args; ret; body } : definition =
+  raise (Failure "bad")
 
-let handle_file (ctx: ctx) (file: file): file =
+let handle_file (ctx : ctx) (file : file) : file =
   let collect_names =
-    List.map (function
-        | Definition (_, dfn) -> InMod(file.name, Base dfn.name)
-        | Type (_, nm, _, _) -> InMod(file.name, Base nm)
-        | Trait (_, dfn) -> InMod(file.name, Base dfn.name)
-        | Impl (_, impl) -> InMod(file.name, Base impl.name)
-      ) file.toplevel
+    List.map
+      (function
+        | Definition (_, dfn) -> InMod (file.name, Base dfn.name)
+        | Type (_, nm, _, _) -> InMod (file.name, Base nm)
+        | Trait (_, dfn) -> InMod (file.name, Base dfn.name)
+        | Impl (_, impl) -> InMod (file.name, Base impl.name))
+      file.toplevel
     |> List.map (fun x -> (x, x))
   in
-  let ctx = {ctx with definitions = collect_names @ ctx.definitions} in
-  let defs = List.map (function
-      | Definition (id, dfn) ->
-        Definition(id, handle_definition ctx dfn)
-      | Type (id, name, args, expr) ->
-        Type(id, name, args, handle_tyexpr ctx expr)
-      | Trait (id, trait) ->
-        Trait(id, handle_trait ctx trait)
-      | Impl (id, impl) ->
-        Impl (id, handle_impl ctx impl)
-    ) file.toplevel
+  let ctx =
+    { ctx with definitions = collect_names @ ctx.definitions }
   in
-  { file with
-    toplevel = defs
-  }
-  
+  let defs =
+    List.map
+      (function
+        | Definition (id, dfn) ->
+            Definition (id, handle_definition ctx dfn)
+        | Type (id, name, args, expr) ->
+            Type (id, name, args, handle_tyexpr ctx expr)
+        | Trait (id, trait) -> Trait (id, handle_trait ctx trait)
+        | Impl (id, impl) -> Impl (id, handle_impl ctx impl))
+      file.toplevel
+  in
+  { file with toplevel = defs }
+
 let handle_files files =
   (* currently handles them in given order
      TODO: implement some sort of proper solver or something
   *)
   let ctx =
-    { definitions = []; included = []; open' = []; current = "" } in
+    { definitions = []; included = []; open' = []; current = "" }
+  in
   let rec go files ctx =
     match files with
     | [] -> []
