d827d7bad7ee69c8ce26e8a07bc49d7d40f2b2d5
jake-87 <jakehfreebsd@gmail.com>
2024-06-09 20:54:43 +1000

    worked further on typechecking; fixed some bugs

--- lib/front/unify.ml
+++ lib/front/unify.ml
@@ -31,15 +31,61 @@ let rec unify' (a : ty) (b : ty) : ty =
       | _ ->
           join_metas a b;
           TyMeta a)
+  | TyMeta a, t | t, TyMeta a -> begin
+      match get a with
+      | Solved b -> unify' t b
+      | Unsolved ->
+          join_metas a (BUR.uref (Solved t));
+          t
+    end
   | Free a, Free b when a = b -> Free a
   | Custom a, Custom b when a = b -> Custom a
   | Tuple w, Tuple e -> Tuple (List.map2 unify' w e)
   | Arrow (a, b), Arrow (q, w) -> Arrow (unify' a q, unify' b w)
   | TApp (a, b), TApp (q, w) when a = q ->
       TApp (q, List.map2 unify' b w)
+  | TForall _, t -> failwith "not sure yet"
   | _ -> raise (BadUnify (a, b))
 
-let unify (a : ty) (b : ty) : (ty, 'a) result =
+let rec inst_frees (mapping : (string * meta BUR.t) list) (ty : ty) :
+    (string * meta BUR.t) list * ty =
+  match force ty with
+  | Custom _ | TyInt | TyChar | TyBool | TyString | TyMeta _ ->
+      (mapping, ty)
+  | Free s -> begin
+      match List.assoc_opt s mapping with
+      | Some s -> (mapping, TyMeta s)
+      | None ->
+          let n = new_meta () in
+          ((s, n) :: mapping, TyMeta n)
+    end
+  | Tuple w ->
+      let map, t =
+        List.fold_left
+          (fun (acc, l) x ->
+            let map, me = inst_frees acc x in
+            (map, me :: l))
+          (mapping, []) w
+      in
+      (map, Tuple t)
+  | Arrow (a, b) ->
+      let map, a = inst_frees mapping a in
+      let map, b = inst_frees map b in
+      (map, Arrow (a, b))
+  | TApp (a, b) ->
+      let map, t =
+        List.fold_left
+          (fun (acc, l) x ->
+            let map, me = inst_frees acc x in
+            (map, me :: l))
+          (mapping, []) b
+      in
+      (map, TApp (a, t))
+  | TForall (f, l) -> failwith "handle scoping issues"
+
+let unify (a : ty) (b : ty) : (unit, 'a) result =
+  let map, a = inst_frees [] a in
+  let map, b = inst_frees map b in
   match unify' a b with
-  | s -> ok s
+  | s -> ok ()
   | exception BadUnify (q, w) -> err @@ `Bad_Unify ((a, q), (b, w))
