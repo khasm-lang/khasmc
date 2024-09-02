9d1be13d12764f8938502547f8103ed95fdbdbc1
Wren H <>
2024-09-02 21:31:13 +1000

    .

--- lib/share/result.ml
+++ lib/share/result.ml
@@ -1,12 +1,9 @@
-let (let*) (x : ('a, 'b) result) (f : 'a -> ('c, 'b) result) =
-  match x with
-  | Ok x -> f x
-  | Error x -> Error x
+let ( let* ) (x : ('a, 'b) result) (f : 'a -> ('c, 'b) result) =
+  match x with Ok x -> f x | Error x -> Error x
 
-let err = fun x -> Error x
-let ok = fun x -> Ok x
+let err x = Error x
+let ok x = Ok x
 
-let (let**) (x: ('a option * string)) (f: 'a -> ('c, string) result) =
-       match fst x with
-       | Some n -> f n
-       | None -> Error (snd x)
+let ( let** ) (x : 'a option * string) (f : 'a -> ('c, string) result)
+    =
+  match fst x with Some n -> f n | None -> Error (snd x)
