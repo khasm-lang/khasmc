d71b6f98f789cb17dabf7a936aab8c9157c01796
Wren H <>
2024-09-04 22:16:46 +1000

    futher typechecker work

--- lib/frontend/typecheck.ml
+++ lib/frontend/typecheck.ml
@@ -1,13 +1,14 @@
 open Share.Uuid
 open Ast
 open Share.Result
+open Share.Maybe
 
 type ctx = {
   (* name, parent *)
   ctors : (resolved * resolved typdef) list;
   types : resolved typdef list;
   traitfuns : (resolved * resolved trait) list;
-  funs : (resolved * resolved definition) list;
+  funs : (resolved * (resolved, no) definition) list;
   locals : (resolved * resolved typ) list;
   local_polys : resolved list;
 }
@@ -22,32 +23,97 @@ let empty_ctx () =
     local_polys = [];
   }
 
+let search (ctx : ctx) (id : resolved) : (resolved typ, string) result
+    =
+  match List.assoc_opt id ctx.locals with
+  | Some ty -> ok ty
+  | None -> (
+      match List.assoc_opt id ctx.ctors with
+      | Some t -> (
+          match t.content with
+          | Record r -> ok @@ TyCustom t.name
+          | Sum s ->
+              let l = List.assoc id s in
+              ok @@ typ_list_to_typ l)
+      | None -> (
+          match List.assoc_opt id ctx.funs with
+          | Some d -> ok @@ definition_type d
+          | None -> (
+              match List.assoc_opt id ctx.traitfuns with
+              | Some t ->
+                  let d =
+                    List.find
+                      (fun (d : ('a, 'b) definition) -> d.name = id)
+                      t.functions
+                  in
+                  ok @@ definition_type d
+              | None -> err ("no such thing: " ^ show_resolved id))))
+
 let type_information : resolved typ by_uuid = new_by_uuid 100
 let add_type uuid typ = Hashtbl.replace type_information uuid typ
 
-let infer (ctx : ctx) (e : resolved expr) :
-    (resolved typ, string) result =
-  failwith "ho"
+let rec break_down_case_pattern (ctx : ctx) (c : resolved case)
+    (t : resolved typ) :
+    ((resolved * resolved typ) list, string) result =
+  match c with
+  | CaseVar v -> ok [ (v, t) ]
+  | CaseTuple tu -> begin
+      match t with
+      | TyTuple t' ->
+          List.map2 (break_down_case_pattern ctx) tu t'
+          |> collect
+          |> Result.map List.flatten
+          |> Result.map_error (String.concat " ")
+      | _ -> err "not tuple but should be tuple :("
+    end
+  | CaseCtor (name, args) -> failwith "ugh"
 
-let check (ctx : ctx) (e : resolved expr) (t : resolved typ) :
+let rec infer (ctx : ctx) (e : resolved expr) :
+    (resolved typ, string) result =
+  let* ty =
+    match e with
+    | Var (i, v) -> search ctx v
+    | Int (_, _) -> ok TyInt
+    | String (_, _) -> ok TyString
+    | Char (_, _) -> ok TyChar
+    | Float (_, _) -> ok TyFloat
+    | Bool (_, _) -> ok TyBool
+    | LetIn (i, case, annot, head, body) -> failwith "let"
+    | Seq (_, _, _) -> _
+    | Funccall (_, _, _) -> _
+    | Binop (_, _) -> _
+    | Lambda (_, _, _, _) -> _
+    | Tuple (_, _) -> _
+    | Annot (_, _, _, _, _) -> _
+    | Match (_, _, _) -> _
+    | Project (_, _, _) -> _
+    | Ref (_, _) -> _
+    | Modify (_, _, _) -> _
+    | Record (_, _) -> _
+  in
+  let uuid = get_uuid e in
+  add_type uuid ty;
+  ok ty
+
+and check (ctx : ctx) (e : resolved expr) (t : resolved typ) :
     (resolved typ, string) result =
   failwith "hi"
 
-let typecheck_definition (ctx : ctx) (d : resolved definition) :
-    (unit, string) result =
+let typecheck_definition (ctx : ctx) (d : (resolved, yes) definition)
+    : (unit, string) result =
   let polys = d.typeargs in
   let args = d.args in
-  let self = (d.name, d) in
+  let self = (d.name, forget_body d) in
   let ctx =
     {
       ctx with
       locals = args;
       local_polys = polys;
-      funs = self :: ctx.funs;
       (* yay recursion *)
+      funs = self :: ctx.funs;
     }
   in
-  let** body = (d.body, "should be body") in
+  let body = get d.body in
   let* _ = check ctx body d.return in
   ok ()
 
@@ -73,7 +139,7 @@ let gather (t : resolved toplevel list) : ctx =
         end
       | Trait t ->
           List.fold_left
-            (fun acc (a : 'a definition) ->
+            (fun acc (a : ('a, 'b) definition) ->
               { acc with traitfuns = (a.name, t) :: acc.traitfuns })
             ctx t.functions
       | Impl _ ->
@@ -81,7 +147,8 @@ let gather (t : resolved toplevel list) : ctx =
              TODO: typecheck impl'd functions
           *)
           ctx
-      | Definition d -> { ctx with funs = (d.name, d) :: ctx.funs })
+      | Definition d ->
+          { ctx with funs = (d.name, forget_body d) :: ctx.funs })
     ctx t
 
 let typecheck_toplevel (t : resolved toplevel list) : unit =
--- lib/share/result.ml
+++ lib/share/result.ml
@@ -7,3 +7,13 @@ let ok x = Ok x
 let ( let** ) (x : 'a option * string) (f : 'a -> ('c, string) result)
     =
   match fst x with Some n -> f n | None -> Error (snd x)
+
+let collect (x : ('a, 'b) result list) : ('a list, 'b list) result =
+  match
+    List.filter (fun x -> match x with Ok _ -> false | _ -> true) x
+  with
+  | [] ->
+      List.map (function Ok s -> s | _ -> failwith "bad") x |> ok
+  | x ->
+      List.map (function Error s -> s | _ -> failwith "bad") x
+      |> err
