04424221fd7dd095f0cd45ba8e00f08ee2f73437
Jake H <jhhenderson87@gmail.com>
2025-12-24 23:03:56 +1000

    Remove traits

--- lib/parsing/ast.ml
+++ lib/parsing/ast.ml
@@ -33,39 +33,11 @@ and 'a typ =
   | TyArrow of 'a typ * 'a typ
   | TyPoly of 'a
   | TyCustom of 'a * 'a typ list
-  | TyAssoc of 'a trait_bound * 'a (* <A as T a b, whatever>::B *)
   | TyRef of 'a typ (* mutability shock horror *)
   | TyMeta of 'a meta ref
 [@@deriving show { with_path = false }]
 
 and 'a field = 'a * 'a typ [@@deriving show { with_path = false }]
-
-and 'a trait_bound =
-  uuid
-  * 'a
-  (* unique id, trait name*)
-  * ('a * 'a typ) list (* args *)
-  * ('a * 'a typ) list (* assocs *)
-[@@deriving show { with_path = false }]
-
-let do_within_trait_bound (fn : 'a typ -> 'a typ) (t : 'a trait_bound)
-    : 'a trait_bound =
-  let uuid, name, args, assoc = t in
-  let snd_f (a, b) = (a, fn b) in
-  (uuid, name, List.map snd_f args, List.map snd_f assoc)
-
-let do_within_trait_bound' (fn : 'a typ -> 'b) (t : 'a trait_bound) :
-    'b list =
-  let uuid, name, args, assoc = t in
-  List.map (fun x -> fn (snd x)) args
-  @ List.map (fun x -> fn (snd x)) assoc
-
-let do_within_trait_bound'2 (fn : 'a typ -> 'a typ -> 'b) q w :
-    'b list =
-  let uuid, name, args1, assoc1 = q in
-  let uuid, name, args2, assoc2 = w in
-  List.map2 (fun x y -> fn (snd x) (snd y)) args1 args2
-  @ List.map2 (fun x y -> fn (snd x) (snd y)) assoc1 assoc2
 
 let rec regeneralize (nw : unit -> 'a) (ty : 'a typ) =
   let f = regeneralize nw in
@@ -73,7 +45,6 @@ let rec regeneralize (nw : unit -> 'a) (ty : 'a typ) =
   | TyTuple t -> TyTuple (List.map f t)
   | TyArrow (a, b) -> TyArrow (f a, f b)
   | TyCustom (c, t) -> TyCustom (c, List.map f t)
-  | TyAssoc (trt, a) -> TyAssoc (do_within_trait_bound f trt, a)
   | TyRef r -> TyRef (f r)
   | TyMeta m ->
       TyMeta
@@ -96,17 +67,7 @@ let rec force (t : 'a typ) : 'a typ =
   | TyMeta m -> begin
       match !m with Unresolved -> t | Resolved t -> force t
     end
-  | TyAssoc (bd, a) -> TyAssoc (do_within_trait_bound force bd, a)
   | _ -> t
-
-type ptr = int64
-
-let ptr_of (x : 'a) : ptr =
-  (* SEGFAULTS IF CALLED ON AN INT *)
-  if Obj.is_int (Obj.repr x) then
-    failwith "ptr_of integer"
-  else
-    Obj.magic x
 
 let get_polys t =
   let rec g t =
@@ -177,7 +138,6 @@ let rec subst_polys (map : ('a * 'a typ) list) (x : 'a typ) : 'a typ =
       match List.assoc_opt k map with Some t -> t | None -> x
     end
   | TyCustom (a, bnd) -> TyCustom (a, List.map f bnd)
-  | TyAssoc (bnd, a) -> TyAssoc (do_within_trait_bound f bnd, a)
   | TyRef r -> TyRef (f r)
   | TyMeta m -> x
   | _ -> x
@@ -191,8 +151,6 @@ let rec match_polys bef aft : 'a list =
   | TyCustom (_, xs), TyCustom (_, ys) ->
       (* we assume it's all correct *)
       List.flatten (List.map2 match_polys xs ys)
-  | TyAssoc (a, _), TyAssoc (b, _) ->
-      List.flatten (do_within_trait_bound'2 match_polys a b)
   | TyRef a, TyRef b -> match_polys a b
   | TyPoly a, b | b, TyPoly a -> [ (a, b) ]
   | a, b ->
@@ -212,8 +170,8 @@ type 'a case =
   | CaseLit of literal
 [@@deriving show { with_path = false }]
 
-type data = {
-  uuid : uuid;
+type 'a data = {
+  uuid : 'a uuid;
   mutable counter : int;
   (* file line col *)
   span : (string * int * int) option;
@@ -229,37 +187,42 @@ type binop =
   | Div
   | LAnd
   | LOr
+  | Lt
+  | Gt
+  | LtEq
+  | GtEq
   | Eq
 [@@deriving show { with_path = false }]
 
-type 'a expr =
-  | Var of data * 'a
+type ('a, 'b) expr =
+  | Var of 'b data * 'a
   (* For monomorphization
      Arguably this should warrant another AST but I don't think
      that's really needed
    *)
-  | MGlobal of data * uuid * 'a
-  | MLocal of data * 'a
-  | Int of data * string
-  | String of data * string
-  | Char of data * string
-  | Float of data * string
-  | Bool of data * bool
-  | LetIn of data * 'a case * 'a typ option * 'a expr * 'a expr
-  | Seq of data * 'a expr * 'a expr
-  | Funccall of data * 'a expr * 'a expr
-  | Binop of data * binop * 'a expr * 'a expr
-  | Lambda of data * 'a * 'a typ option * 'a expr
-  | Tuple of data * 'a expr list
-  | Annot of data * 'a expr * 'a typ
-  | Match of data * 'a expr * ('a case * 'a expr) list
-  | Project of data * 'a expr * int
-  | Ref of data * 'a expr
-  | Modify of data * 'a * 'a expr
-  | Record of data * 'a * ('a * 'a expr) list
+  | MGlobal of 'b  data * ('a typ) uuid * 'a
+  | MLocal of 'b data * 'a
+  | Int of 'b data * string
+  | String of 'b data * string
+  | Char of 'b data * string
+  | Float of 'b data * string
+  | Bool of 'b data * bool
+  | LetIn of 'b data * 'a case * 'a typ option *
+               ('a, 'b) expr * ('a, 'b) expr
+  | Seq of 'b data * ('a, 'b) expr * ('a, 'b) expr
+  | Funccall of 'b data * ('a, 'b) expr * ('a, 'b) expr
+  | Binop of 'b data * binop * ('a, 'b) expr * ('a, 'b) expr
+  | Lambda of 'b data * 'a * 'a typ option * ('a, 'b) expr
+  | Tuple of 'b data * ('a, 'b) expr list
+  | Annot of 'b data * ('a, 'b) expr * 'a typ
+  | Match of 'b data * ('a, 'b) expr * ('a case * ('a, 'b) expr) list
+  | Project of 'b data * ('a, 'b) expr * int
+  | Ref of 'b data * ('a, 'b) expr
+  | Modify of 'b data * 'a * ('a, 'b) expr
+  | Record of 'b data * 'a * ('a * ('a, 'b) expr) list
 [@@deriving show { with_path = false }]
 
-let get_uuid (e : 'a expr) : uuid =
+let get_uuid (e : ('a, 'b) expr) : 'b uuid =
   match e with
   | MLocal (i, _)
   | MGlobal (i, _, _)
@@ -327,15 +290,15 @@ let rec typ_list_to_typ (t : 'a typ list) : 'a typ =
   | x :: xs -> TyArrow (x, typ_list_to_typ xs)
 
 (* TODO: support GADTs*)
-type 'a typdef = {
-  data : data;
+type ('a, 'b) typdef = {
+  data : 'b data;
   name : 'a;
   args : 'a list;
   content : 'a typdef_case;
 }
 [@@deriving show { with_path = false }]
 
-let typdef_and_ctor_to_typ (t : 'a typdef) (i : 'a) : 'a typ =
+let typdef_and_ctor_to_typ (t : ('a, 'b) typdef) (i : 'a) : 'a typ =
   match t.content with
   | Record _ -> failwith "shouldn't be record"
   | Sum s ->
@@ -345,52 +308,26 @@ let typdef_and_ctor_to_typ (t : 'a typdef) (i : 'a) : 'a typ =
       in
       typ_list_to_typ (ctor @ [ custom ])
 
-type ('a, 'p) definition = {
-  data : data;
+type ('a, 'b, 'p) definition = {
+  data : 'b data;
   name : 'a;
   typeargs : 'a list;
   args : ('a * 'a typ) list;
-  bounds : 'a trait_bound list;
   return : 'a typ;
   (* essentially for cross-compatibility with other structures *)
-  body : ('a expr, 'p) maybe;
+  body : (('a, 'b) expr, 'p) maybe;
 }
 [@@deriving show { with_path = false }]
 
-let forget_body : ('a, yes) definition -> ('a, no) definition =
+let forget_body : ('a, 'b, yes) definition -> ('a, 'b, no) definition =
  fun x -> { x with body = Nothing }
 
-type 'a trait = {
-  data : data;
-  name : 'a;
-  args : 'a list;
-  assocs : 'a list;
-  requirements : 'a trait_bound list;
-  functions : ('a, no) definition list;
-}
-[@@deriving show { with_path = false }]
-
-type 'a impl = {
-  (* TODO: support stuff of the form impl<A> T<A>*)
-  data : data;
-  parent : 'a;
-  args : ('a * 'a typ) list;
-  assocs : ('a * 'a typ) list;
-  (* we give each impl function a unique id alongside it's
-     name predefined from the trait
-  *)
-  impls : (uuid * ('a, yes) definition) list;
-}
-[@@deriving show { with_path = false }]
-
-let definition_type (type a) (d : ('a, a) definition) : 'a typ =
+let definition_type (type a) (d : ('a, 'b, a) definition) : 'a typ =
   List.fold_right
     (fun (_, ty) acc -> TyArrow (ty, acc))
     d.args d.return
 
-type 'a toplevel =
-  | Typdef of 'a typdef
-  | Trait of 'a trait
-  | Impl of 'a impl
-  | Definition of ('a, yes) definition
+type ('a, 'b) toplevel =
+  | Typdef of ('a, 'b) typdef
+  | Definition of ('a, 'b, yes) definition
 [@@deriving show { with_path = false }]
--- lib/share/uuid.ml
+++ lib/share/uuid.ml
@@ -1,25 +1,30 @@
-type uuid = UUID of (int * int)
+type 'a uuid = UUID of (int * 'a)
 [@@deriving show { with_path = false }]
 
-let new_by_uuid n : (uuid, 'a) Hashtbl.t = Hashtbl.create n
+let new_by_uuid n : ('a uuid, 'b) Hashtbl.t = Hashtbl.create n
 let uuid_orig (UUID (a, b)) = UUID (a, 0)
 
 let uuid_by_orig tbl (UUID (a, b)) =
   Hashtbl.find_opt tbl (UUID (a, 0))
 
-let print_by_uuid show p =
+let print_by_uuid show showsnd p =
   let s = Hashtbl.to_seq p in
   s
-  |> Seq.map (fun (a, b) -> show_uuid a ^ ": " ^ show b)
+  |> Seq.map (fun (a, b) -> show_uuid showsnd a ^ ": " ^ show b)
   |> List.of_seq
   |> String.concat "\n"
   |> print_endline
 
-let uuid =
+let uuid : unit -> unit uuid =
   let x = ref 1000 in
   fun () ->
     incr x;
-    UUID (!x, 0)
+    UUID (!x, ())
+
+let uuid_forget (v : 'a uuid) : unit uuid =
+  let UUID (a, b) = v in
+  UUID (a, ())
+
 
 let uuid_set_version v uuid =
   let (UUID (a, b)) = uuid in
